f47c70d3d0be282a0408fd693f30e6728b89342c
Daragh Byrne <daragh99@gmail.com>
2020-05-08 18:51:14 +1000

    tidy

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const httpPort = process.env.PORT || 5000;
 /**
  * Start express to serve the React client
  */
-
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
